2071960758949189a0d103c41ebde409954e3699
Mateusz Jankowski <mat.jankowski@gmail.com>
2026-04-06 10:54:51 +0300

    Simplify config: always read from config.js, no fetch

--- upload_config.sh
+++ upload_config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
-# Generate config.js from config.json
-echo "window.WORKOUT_CONFIG = $(cat config.json);" > config.js
-
-git add config.json config.js
+git add config.js
 git commit -m "Update workout config"
 git push origin main
