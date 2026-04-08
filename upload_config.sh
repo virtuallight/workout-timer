89197d24a6962e199989ae78517c8235e776a343
Mateusz Jankowski <mat.jankowski@gmail.com>
2026-04-08 17:43:16 +0300

    update

--- upload_config.sh
+++ upload_config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-git add config.js
+echo "window.WORKOUT_CONFIG = $(cat config.json);" > config.js
+git add config.json config.js
 git commit -m "Update workout config"
 git push origin main
